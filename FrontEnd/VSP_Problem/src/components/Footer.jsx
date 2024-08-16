466c437d75ac5a6d81089caf67251c0707d037b5
Rahul Harpal <51887323+rahulharpal1603@users.noreply.github.com>
2024-08-16 18:39:57 +0530

    Update Footer.jsx

--- FrontEnd/VSP_Problem/src/components/Footer.jsx
+++ FrontEnd/VSP_Problem/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="bg-slate-700 mt-auto">
       <div className='flex flex-col md:flex-row justify-between'>
         <div className='m-2 flex flex-row justify-center'>
-          <h1 className='text-white m-3 text-lg'>&copy; Developed By Team</h1>
+          <h1 className='text-white m-3 text-lg'>&copy; Developed By CodeCrafters</h1>
         </div>
         <div className='m-5 flex flex-row justify-center'>
         </div>
